43efb82a374bd89bb9e7a450938c1ba002e84e23
Stegura Alex <stegura.alex@gmail.com>
2018-11-22 16:03:30 +0300

    Создание объекта Vector

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// Создаем сласс Vector
+class Vector {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Создаем метод plus()
+    plus(vector) {
+        try {
+            if (!(vector instanceof Vector)) {
+                throw 'Можно прибавлять к вектору только вектор типа Vector';
+            }
+            return new Vector(vector.x + this.x, vector.y + this.y);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    // Создаем метод times()
+    times(num) {
+        return new Vector(this.x * num, this.y * num);
+    }
+}
